4c946d999de27d9501cbd52d75ca1ab08eea068c
iTzFreeHD <jaaptijmes58@gmail.com>
2021-02-03 13:28:28 +0100

    Uploading formBridge for PMMP

---
+++ src/formBridge/Main.php
@@ -0,0 +1,11 @@
+<?php
+namespace formBridge;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+    public function onEnable()
+    {
+        $this->getServer()->getLogger()->info("FormBridge-1 by iTzFreeHD -> [Altay] is started");
+    }
+
+}
